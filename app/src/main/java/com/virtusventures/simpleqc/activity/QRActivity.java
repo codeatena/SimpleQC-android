87d9ddf4cff83dd96bcfa0ffcdeb501c79145a58
codeatena <codeatena@yahoo.com>
2017-03-01 21:01:47 +0800

    android 6.0 permission fix

--- app/src/main/java/com/virtusventures/simpleqc/activity/QRActivity.java
+++ app/src/main/java/com/virtusventures/simpleqc/activity/QRActivity.java
@@ -1,8 +1,13 @@
 package com.virtusventures.simpleqc.activity;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 
 import com.google.zxing.Result;
 
@@ -24,7 +29,16 @@ public class QRActivity extends Activity implements ZXingScannerView.ResultHandl
     public void onResume() {
         super.onResume();
         mScannerView.setResultHandler(this); // Register ourselves as a handler for scan results.
-        mScannerView.startCamera();          // Start camera on resume
+
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1) {
+            if (!checkIfAlreadyhavePermission()) {
+                requestForSpecificPermission();
+            }
+            else
+                mScannerView.startCamera();          // Start camera on resume
+        }
+        else
+            mScannerView.startCamera();          // Start camera on resume
     }
 
     @Override
@@ -43,4 +57,32 @@ public class QRActivity extends Activity implements ZXingScannerView.ResultHandl
         finish();
     }
 
+    private boolean checkIfAlreadyhavePermission() {
+        int result = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+        if (result == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private void requestForSpecificPermission() {
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 101);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case 101:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //granted
+                    mScannerView.startCamera();
+                } else {
+                    //not granted
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
 }
